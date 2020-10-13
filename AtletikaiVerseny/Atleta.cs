59502b542d5b19f16cd49d2d1535b726aa8c0dff
ErdosXenia <erdos.xenia@gmail.com>
2020-10-13 08:14:42 +0200

    Elso feladat

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaiVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get;private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+
+        public Atleta(string sor)
+        {
+            string[] a = sor.Split(';');
+            Rajtszam = a[0];
+            VezNev = a[1];
+            KerNev = a[2];
+            Egyesulet = a[3];
+
+            //string[] Nev = new string[] { };
+            //Nev = VezNev + KerNev;
+        }
+
+    }
+}
--- AtletikaiVerseny/Program.cs
+++ AtletikaiVerseny/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AtletikaiVerseny
 {
     class Program
     {
+        static List<Atleta> atletak = new List<Atleta>();
+
+        static void feladat1()
+        {
+            StreamReader sr = new StreamReader("tavol.csv");
+            while (!sr.EndOfStream)
+            {
+                atletak.Add(new Atleta(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
         static void Main(string[] args)
         {
+            feladat1();
+
+            Console.ReadKey();
         }
     }
 }
